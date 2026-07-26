a192fd12ba6df3eb5b3671ff65c508e830e062e7
wavemoroc001 <game55544@hotmail.com>
2026-07-27 00:14:21 +0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 1
+print(type(variable))
+variable=3.14
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Hello World"
+print(type(variable))
+variable= [1,2,3]
+print(type(variable))
