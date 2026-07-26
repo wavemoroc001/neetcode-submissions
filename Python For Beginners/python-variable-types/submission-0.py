c65be7f6d6b953ae5d451b06fd5e55627d747a33
wavemoroc001 <game55544@hotmail.com>
2026-07-27 00:12:11 +0700

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,12 @@
+integer_type = 9
+float_type = 3.14
+bool_type = True
+string_type = "Hello World!"
+list_type = [1,2,3]
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
